08d962576b9792e942aef732c6418bd7d9448990
Max Nägele <max@naegele.io>
2022-02-16 18:11:19 +0100

    Added HTTP and HTTPS support

--- redirect.js
+++ redirect.js
@@ -1,7 +1,7 @@
-if (location.hostname !=
-document.currentScript.getAttribute('domain')){
-  location.href = "http://" +
-document.currentScript.getAttribute('domain') + location.pathname;
-} else{
-  console.log("Already Redirected");
+const domain = document.currentScript.getAttribute('domain');
+if (location.hostname != domain) {
+  console.info(`Redirecting to the HNS domain ${domain}!`);
+  location.href = `${location.protocol}//${domain}${location.pathname}`;
+} else {
+  console.info('Already using HNS domain, no redirect needed!');
 }
