3cb70343293f2ceccce6464dddc4d6cc6c485f5b
Max Nägele <max@naegele.io>
2022-02-16 18:10:42 +0100

    HTTP and HTTP can be done in one script. See redirect.js

--- handshake.js
+++
@@ -1,7 +0,0 @@
-if (location.hostname !=
-document.currentScript.getAttribute('domain')){
-  location.href = "https://" +
-document.currentScript.getAttribute('domain') + location.pathname;
-} else{
-  console.log("Already Redirected");
-}
