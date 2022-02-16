5b7ca99592054ac7d9d3a6ed4d12d1ba8ab2362e
Nathan Woodburn <contact@nathan.woodburn.id.au>
2022-02-16 21:32:05 +1100

    Update https.js

--- https.js
+++ https.js
@@ -1,6 +1,5 @@
 if (location.protocol !== 'https:') {
-    location.replace(`https:${location.href.substring(location.protocol.length)}`);
-}
-else{
-console.log("Already Https");
+  location.replace(`https:${location.href.substring(location.protocol.length)}`);
+} else{
+  console.log("Already Https");
 }
