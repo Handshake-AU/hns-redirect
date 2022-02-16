ad50640b14f77391d2cdfb6563f873b79a77b9f2
Nathan Woodburn <contact@nathan.woodburn.id.au>
2022-02-16 21:44:44 +1100

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,5 +1,7 @@
-if (location.hostname != document.currentScript.getAttribute('domain')){
-  location.href = "http://" + document.currentScript.getAttribute('domain') + location.pathname;
+if (location.hostname !=
+document.currentScript.getAttribute('domain')){
+  location.href = "http://" +
+document.currentScript.getAttribute('domain') + location.pathname;
 } else{
   console.log("Already Redirected");
 }
